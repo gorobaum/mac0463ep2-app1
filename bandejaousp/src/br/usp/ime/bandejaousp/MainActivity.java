490f2f6dab45f9302ba1e93cb222a8d64e66deeb
Samuel <samuel@euclides.(none)>
2013-06-01 14:30:40 -0300

    commit inicial para adicionar os arquivos

--- bandejaousp/src/br/usp/ime/bandejaousp/MainActivity.java
+++ bandejaousp/src/br/usp/ime/bandejaousp/MainActivity.java
@@ -1,15 +1,28 @@
 package br.usp.ime.bandejaousp;
 
-import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		GoogleMap mMap;
+		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		mMap.addMarker(new MarkerOptions()
+		        .position(new LatLng(0, 0))
+		        .title("Hello world"));
 	}
 
 	@Override
