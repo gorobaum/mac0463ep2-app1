229712f15be72e98f3dacf3bb0446ae3a596ac58
Samuel <samuel@euclides.(none)>
2013-06-03 06:05:56 -0300

    tinha esquecido a classe da atividade RestaurantInfo :P

---
+++ bandejaousp/src/br/usp/ime/bandejaousp/RestaurantInfoActivity.java
@@ -0,0 +1,13 @@
+package br.usp.ime.bandejaousp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RestaurantInfoActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_restaurant_info);
+	}
+}
