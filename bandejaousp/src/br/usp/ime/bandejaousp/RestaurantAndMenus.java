ec4fb68b0e0bb11051723a201ac44eb31be2cfb2
Samuel <samuel@euclides.(none)>
2013-06-02 23:05:00 -0300

    RestaurantAndMenus finalmente num formato que funciona com o json do serviço

--- bandejaousp/src/br/usp/ime/bandejaousp/RestaurantAndMenus.java
+++ bandejaousp/src/br/usp/ime/bandejaousp/RestaurantAndMenus.java
@@ -2,25 +2,46 @@ package br.usp.ime.bandejaousp;
 
 public class RestaurantAndMenus {
 	
-	private Restaurant restaurant;
-	private Menu menus[];
+	/* A estrutura da classe é totalmente definida pelo JSON do serviço do bandejão */
+	
+	/* Um "layer" em volta do restaurante e do menu, devido à estrutura do JSON do serviço */
+	public class RestHolder {
+		private Restaurant restaurant;
+	}
+	
+	public class MenuHolder{
+		private Menu menu;
+	}
+	
+	private RestHolder restaurant;
+	private MenuHolder menus[];
+	private Menu actualMenus[] = null;
 	
 	RestaurantAndMenus() {
 		this(null, null);
 	}
 	
-	RestaurantAndMenus(Restaurant restaurant, Menu menus[]) {
+	RestaurantAndMenus(RestHolder restaurant, MenuHolder menus[]) {
 		super();
 		this.restaurant = restaurant;
 		this.menus = menus;
 	}
 
 	public Restaurant getRestaurant() {
-		return restaurant;
+		return restaurant.restaurant;
 	}
 
 	public Menu[] getMenus() {
-		return menus;
+		if (actualMenus == null)
+			menusToActualMenus();
+		return actualMenus;
+	}
+	
+	private void menusToActualMenus() {
+		actualMenus = new Menu[menus.length];
+		for (int i = 0; i < menus.length; i++) {
+			actualMenus[i] = menus[i].menu;
+		}
 	}
 	
 }
